18d397f97a1c60d94148e530727c37689f74d835
ucpr <contact@ucpr.dev>
2023-06-07 15:56:04 +0900

    fix tool repository

--- lib/utils.bash
+++ lib/utils.bash
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for circleci-cli.
-GH_REPO="https://github.com/ucpr/circleci-cli"
+GH_REPO="https://github.com/CircleCI-Public/circleci-cli"
 TOOL_NAME="circleci-cli"
 TOOL_TEST="circleci-cli --help"
 
